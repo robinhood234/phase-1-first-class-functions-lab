3ec2affde92c14a8749539333dbc0f73f412c292
robin nganga smith <saintofcookies.2021@gmail.com>
2023-10-03 11:20:09 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+//drivers = ['robin', 'patrick', 'Gidy Mane', 'Vera Obiera', 'mum'];
+const returnFirstTwoDrivers = function (drivers) {
+ return drivers.slice(0, 2);
+};
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (integer) {
+return function  (fare) {
+return fare * integer;
+}
+}
+const fareDoubler = createFareMultiplier (2);
+//const doubleFare = fareDoubler (fare);
+
+const fareTripler = createFareMultiplier (3);
+//const fare = integer;
+//const tripleFare = fareTripler (fare);
+
+function selectDifferentDrivers(drivers, returnFirstTwoDrivers) {
+return returnFirstTwoDrivers (drivers);
+}
